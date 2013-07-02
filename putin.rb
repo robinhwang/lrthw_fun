a04be3b6d567ee07de3c1da4d9ba7b30d0f1d8b1
robinhwang <binbinhuangcsu@gmail.com>
2013-07-02 10:51:37 +0800

    gets.chomp;

---
+++ putin.rb
@@ -0,0 +1,14 @@
+# print "How old are you "
+# age = gets.chomp()
+# print "How tall are you "
+# height = gets.chomp()
+# print "how much you weigh "
+# weight = gets.chomp()
+
+
+# puts "So , you're #{age} years old, #{height} tall and #{weight} heavy"
+
+print "somebody wanna know a number"
+number = gets.chomp()
+
+puts "so you get a #{number}"
